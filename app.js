f10e03bf1324f00e3c1f2109a921aedaf2287ad5
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-03-08 20:11:18 +0000

    mark

---
+++ app.js
@@ -0,0 +1,20 @@
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function generater(red){
+    return Math.floor(Math.random() * who.length)
+}
+
+// var pool = who[Math.floor(Math.random() * who.length)];
+// var second = action[Math.floor(Math.random() * action.length)];
+// var third = what[Math.floor(Math.random() * what.length)];
+// var fourth = when[Math.floor(Math.random() * when.length)];
+
+console.log(`${who[generater(who)]} ${action[generater(action)]} ${what[generater(what)]} ${when[generater(when)]}`)
